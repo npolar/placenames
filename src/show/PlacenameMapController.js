deb8de648f476d41cb406b3e764329d2bcf5f9f3
cnrdh <cnrdh@users.noreply.github.com>
2017-04-03 10:33:25 +0200

    UI upgrade; map for historical names with no position; support legacy URLs name?ident=

--- src/show/PlacenameMapController.js
+++ src/show/PlacenameMapController.js
@@ -1,4 +1,5 @@
 'use strict';
+
 function PlacenameMapController($scope, $controller, $location, $routeParams, $timeout, $filter,
   NpolarEsriLeaflet, NpolarMessage,
   PlacenameResource,
@@ -10,7 +11,19 @@ function PlacenameMapController($scope, $controller, $location, $routeParams, $t
   // Placename -> npolarApiResource -> ngResource
   $scope.resource = PlacenameResource;
 
-  self.getPoint = (p, includeAltitude=true) => {
+  $controller('NpolarBaseController', {
+    $scope: $scope
+  });
+
+  self.mapUri = (p) => {
+    let uri = '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+    if ('Svalbard' === p.area) {
+      uri = '//geodata.npolar.no/arcgis/rest/services/Basisdata/NP_Satellitt_Svalbard_WMTS_25833/MapServer';
+    }
+    return uri;
+  };
+
+  self.getPoint = (p, includeAltitude=false) => {
     let point;
     if (p.latitude && p.longitude) {
       point = [p.latitude, p.longitude];
@@ -25,22 +38,34 @@ function PlacenameMapController($scope, $controller, $location, $routeParams, $t
         } else {
           point = [coordinates[1], coordinates[0]];
         }
-
       }
     }
     return point;
   };
 
-  self.drawNamesinBox = (map=self.map) => {
+  self.popup = (p, current=$routeParams.id) => {
+    let name = (p.id !== current) ? `<a href="${p.id}">${p.name['@value']}</a>` : p.name['@value'] ;
+    let def = $filter('i18n')(p.texts.definition) || '';
+    let origin = $filter('i18n')(p.texts.origin) || '';
+    let orig = origin.substring(0,300);
+    if (orig !== origin) {
+      orig += ' […]';
+    }
+    let coords = JSON.stringify(self.getPoint(p, true));
+    let popup = `<b>${name}</b><br/>${ p.area}, ${p.country}<br/>${ coords }<br/><br/>${ def }<br/><br/>${ orig }`;
+    return popup;
+  };
+
+  self.drawNamesinBox = (map=self.map, L=NpolarEsriLeaflet.L()) => {
     let [w,s,e,n] = map.getBounds().toBBoxString().split(',');
+    console.debug([w,s,e,n]);
     let names_in_bbox_query = { q:'', 'filter-longitude': `${w}..${e}`, 'filter-latitude': `${s}..${n}`,
-      limit: 'all', 'not-id': $routeParams.id, format: 'geojson', fields: 'id,type,geometry,latitude,longitude,name.@value,texts.definition,texts.origin'
+      limit: 'all', 'not-id': $routeParams.id, format: 'geojson', fields: 'id,type,area,country,geometry,latitude,longitude,name.@value,texts'
     };
     PlacenameResource.feed(names_in_bbox_query).$promise.then(r => {
       console.log('bbox names', r.features.length);
       r.features.forEach(p => {
-        let popup = `<a href="${p.id}"><b>${p.name['@value']}</b></a><br/>${ $filter('i18n')(p.texts.definition||'') }<br/>${ $filter('i18n')(p.texts.origin||'') }`;
-        L.circle([p.latitude, p.longitude], { radius: 300 }).bindPopup(popup).addTo(self.map);
+        L.circle([p.latitude, p.longitude], { radius: 300, weight: 1 }).bindPopup(self.popup(p)).addTo(self.map);
       });
 
     });
@@ -48,11 +73,11 @@ function PlacenameMapController($scope, $controller, $location, $routeParams, $t
 
   self.renderMap = (p={ area: 'Svalbard'}, L=NpolarEsriLeaflet.L()) => {
     if (!self.map) {
-      self.map = NpolarEsriLeaflet.mapFor('Svalbard');
+      self.map = NpolarEsriLeaflet.mapFactory(self.mapUri(p));
     }
 
     let map = self.map;
-    let config =  NpolarEsriLeaflet.configFor(p.area);
+    let config =  NpolarEsriLeaflet.configFor(p.area, { element: 'np-placenames-show-map'});
 
     let attribution = `<a href="http://npolar.no">Norsk Polarinstitutt</a>`;
     map.attributionControl.addAttribution(attribution);
@@ -61,16 +86,33 @@ function PlacenameMapController($scope, $controller, $location, $routeParams, $t
       self.drawNamesinBox();
     });
 
-
+    // Create large circle for name
     if (p) {
       let point = self.getPoint(p);
-      if (point && point.map(c => parseInt(c) !== [0,0])) {
+      // ... but not if the position is [0,0]
+      if (point && point[0] && point[1] && point[0].to_i !== 0 && point[1].to_i !== 0) {
         map.setView(point, config.zoom);
-        let circle = L.circle(point, { radius: 1000 });
-        circle.bindPopup(`<b>${p.name['@value']}</b><br />${p.area}, ${p.country}`).addTo(map).openPopup();
+        let circle = L.circle(point, { radius: 1000, color: 'red', weight: 1 });
+        circle.bindPopup(self.popup(p)).addTo(map).openPopup();
       } else {
-        console.debug(config);
-        map.setView(config.view, config.zoom);
+        // if [0,0] try to find position of newer name
+        if (p.relations && p.relations.replaced_by) {
+          let replaced_by_id = p.relations.replaced_by[0]['@id'];
+          if (/^http/.test(replaced_by_id)) {
+            replaced_by_id = replaced_by_id.split('/').pop();
+          }
+          PlacenameResource.get({id: replaced_by_id}).$promise.then(replaced_by => {
+            point = self.getPoint(replaced_by);
+            map.setView(point, config.zoom);
+            let circle = L.circle(point, { radius: 1000, color: 'red', weight: 1 });
+            circle.bindPopup(`<b>${replaced_by.name['@value']}</b><br />${replaced_by.area}, ${replaced_by.country}`).addTo(map).openPopup();
+          });
+
+        } else {
+          map.setView(config.view, config.zoom);
+        }
+
+
       }
     }
   };
