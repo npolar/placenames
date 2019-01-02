9e7504bf9beaa986d826fa90c510625b1dbb8968
cnrdh <conrad@npolar.no>
2019-01-02 14:16:13 +0100

    Fixed supercluster #2 (invalid GeoJSON remains)

--- demo/supercluster/worker.js
+++ demo/supercluster/worker.js
@@ -1,42 +1,59 @@
 'use strict';
 
-importScripts('https://unpkg.com/supercluster@2.3.0/dist/supercluster.min.js');
-var now = Date.now();
-var index;
-var uri = '//api.npolar.no/placename/?q=&filter-status=official&format=geojson&fields=name.@value,type,geometry,id,texts&sort=-area&limit=all';
-//var uri = 'np-placenames.json';
+importScripts('https://unpkg.com/supercluster@5');
 
-getJSON(uri, function (geojson) {
-  console.log('loaded ' + geojson.features.length + ' points GeoJSON in ' + ((Date.now() - now) / 1000) + 's');
+const getSuperclusterIndex = () => self.idx;
 
-  index = supercluster({
-      log: true,
-      radius: 80,
-      extent: 256,
-      minZoom: 3,
-      maxZoom: 10
-  }).load(geojson.features);
-  //console.log(index.getTile(0, 0, 0));
-  postMessage({ready: true});
-});
+const getClusters = (bbox,zoom) => getSuperclusterIndex().getClusters(bbox, zoom);
 
-// Handle message containing new bbox/zoom from main thread
+function createSuperclusterIndex(geojson, options={
+    log: true,
+    radius: 80,
+    extent: 256,
+    minZoom: 3,
+    maxZoom: 10
+}) {
+  const supercluster = new Supercluster(options);
+  self.idx = supercluster.load(geojson.features);
+}
+
+async function fetchGeoJSON() {
+  const fields= "&fields=name.@value,type,geometry,id,texts,longitude,latitude";
+  const uri = `https://api.npolar.no/placename/?q=&filter-status=official&format=geojson${fields}&limit=all&filter-latitude=-90..90&&filter-longitude=-180..180`;
+  const timer = `Placenames API: ${uri}`;
+  return fetch(uri).then(r => r.json()).then(fc => fixGeoJSON(fc));
+}
+
+function fixGeoJSON(fc) {
+  const noGeo = fc.features.filter(f => !f.geometry);
+  console.warn('No geometry', JSON.stringify(noGeo.map(f => [f.id,f['name']['@value']])));
+
+  let features = fc.features.filter(f => (f.geometry && f.geometry.coordinates && f.geometry.coordinates.length));
+  if (noGeo && noGeo.length) {
+    features=features.concat(noGeo.map(f => {
+      f.geometry = { type: "Point", coordinates: [f.longitude, f.latitude]}
+      return f;
+    }));
+  }
+  fc.features = features.map(f => {
+    const coordinates = f.geometry.coordinates.map(c => +c.toPrecision(8));
+    f.geometry.coordinates = coordinates;
+    return f;
+  });
+  return fc;
+}
+
+// Receive message with bbox/zoom; send back clusters
 self.onmessage = function (e) {
-  if (e.data) {
-    // Send back clusters for bbox/zoom
-    postMessage(index.getClusters(e.data.bbox, e.data.zoom));
+  if (e.data && e.data.bbox && e.data.zoom) {
+    const { bbox, zoom } = e.data;
+    const clusters = getClusters(bbox,zoom);
+    postMessage(clusters);
   }
 };
 
-function getJSON(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onload = function () {
-    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300 && xhr.response) {
-      callback(xhr.response);
-    }
-  };
-  xhr.send();
-}
+(async () => {
+  const fc = await fetchGeoJSON();
+  createSuperclusterIndex(fc);
+  postMessage({ready: true});
+})();
