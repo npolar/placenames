66c87325e50e1836e66c198f2b0b51c34a6381f2
cnrdh <conrad@npolar.no>
2019-09-11 09:55:31 +0200

    Clean slate

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var npdcGulp = require('npdc-gulp');
-var config = npdcGulp.baseConfig;
-var package = require('./package.json');
-config.COMMON_VERSION = package.dependencies['npdc-common'].split("npdc-common#v")[1];
-npdcGulp.loadAppTasks(gulp, config);
